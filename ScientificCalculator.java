06e769ea68f7e84b62849c52321ea592d838ea86
yashavitelang <91070190+yashavitelang@users.noreply.github.com>
2023-08-26 12:12:50 +0530

    Add files via upload

---
+++ ScientificCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ScientificCalculator;
+
+/**
+ *
+ * @author Dell
+ */
+public class ScientificCalculator {
+   
+    public static void main(String[] args) {
+        CalculatorUI scientific = new CalculatorUI();
+        scientific.setVisible(true);
+    }
+}
+
